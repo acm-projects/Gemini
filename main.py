e79f0e716c88ebb48a5cb08b7217efb5352e82ab
Anushka Pimple <70924549+anushkajp@users.noreply.github.com>
2020-11-08 18:22:23 -0600

    sample api that returns a string query

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route('/api', methods=['GET'])
+def hello_world():
+    d={}
+    d['Query'] = str(request.args['Query'])
+    return jsonify(d)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
